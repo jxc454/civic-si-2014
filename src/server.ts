28adfc5165d0e652769da2d9d4be1d199a85c38d
Jameson Cotter <jameson.cotter@gmail.com>
2019-10-14 11:31:36 -0400

    adding server skeleton

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { ApolloServer } from 'apollo-server-express'
+import Express from 'express'
+import { buildSchema } from 'type-graphql'
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+
+const main = async () => {
+    await createConnection()
+
+    const PORT = 4000
+    const schema = await buildSchema({
+        resolvers: []
+    })
+
+    const apolloServer = new ApolloServer({ schema })
+
+    const app = Express()
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(PORT, () => console.log(`listening on port ${PORT}...`))
+}
+
+main().then()
