b8424deb66a029506b3f51b57f13a2d129356d4b
Jameson Cotter <jameson.cotter@gmail.com>
2019-12-01 14:57:44 -0500

    adding .env to client, for the api endpoint.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,7 +7,7 @@ import Mileage from './Mileage';
 class App extends React.PureComponent {
     render() {
         const client = new ApolloClient({
-            uri: 'http://localhost:4000/graphql',
+            uri: process.env.CAR_LOG_API
         })
 
         client
